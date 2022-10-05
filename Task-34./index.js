240549efcddd77ff00f4f5120970676e3b0ae883
Shah Meer Ali(Sensei) <101212857+Shahmeerali723@users.noreply.github.com>
2022-10-05 04:29:19 -0700

    Add files via upload

---
+++ Task-34./index.js
@@ -0,0 +1,8 @@
+/* Printing out the list of pizzas in the array. */
+
+
+const pizza = ['pepperoni', 'tikka', 'veggie' , 'supreme', 'cheese']
+console.log(
+    "I seriously very much love pizza that is mild in spice and has some juicy stuff and filled crust, such as"
+);
+pizza.map((idx) => console.log(`I really love ${idx}`));
