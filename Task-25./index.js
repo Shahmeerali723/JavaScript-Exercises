2d9a994095469357e7d7a4a8955a7de41670894f
Shah Meer Ali(Sensei) <101212857+Shahmeerali723@users.noreply.github.com>
2022-10-05 04:20:24 -0700

    Add files via upload

---
+++ Task-25./index.js
@@ -0,0 +1,9 @@
+/* The above code is checking the color of the alien and if the color is green, the player will earn 5
+points. */
+
+
+let alien_color = "green";
+if (alien_color === "green")
+console.log("player just earned 5 points");
+if (alien_color === "red")
+console.log("if you are seeing this that means i am wrong");
