e993e495f05fb5737b3e0b19244d922e8d609d89
Shah Meer Ali(Sensei) <101212857+Shahmeerali723@users.noreply.github.com>
2022-10-05 04:00:43 -0700

    Add files via upload

---
+++ Task-10./index.js
@@ -0,0 +1,10 @@
+/* The above code is declaring a variable called "now" and giving it the value of the date function. */
+
+
+let name = "Shah Meer"; // Declare variable :name, give it the value of "Madiha"
+console.log(`"Hello, ${name} ,would you like to learn javascript today?"`);
+//print name with  message
+
+
+let now = new Date(); //Declare variable :"now", give it the value of date function
+ alert( now ); // shows current date/time
