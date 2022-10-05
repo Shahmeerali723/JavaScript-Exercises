4161c725b23a59007f4239b132e0ba7e1c434af4
Shah Meer Ali(Sensei) <101212857+Shahmeerali723@users.noreply.github.com>
2022-10-05 04:07:28 -0700

    Add files via upload

---
+++ Task-12./index.js
@@ -0,0 +1,4 @@
+/* Using the template literal to print the names in the array. */
+
+const names = ["Shah Meer", "Sulman", "Laraib", "Qalb-E-Momin"];
+names.map((name) => console.log(`Hello ${name}, Today i'm attending a Metaverse class is too much knowledgeable.`));
