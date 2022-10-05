eeaccee35515ec61f5dcb9d2fdc48a5b92649e75
Shah Meer Ali(Sensei) <101212857+Shahmeerali723@users.noreply.github.com>
2022-10-05 03:42:02 -0700

    Add files via upload

---
+++ Task-02./index.js
@@ -0,0 +1,6 @@
+/* Storing the name Eric in a variable and printing a message to that person. */
+
+let person = "Syed Shah Meer Ali";
+console.log(`"Hello," ${person}, " would you like to learn some Python today?"`);
+
+
