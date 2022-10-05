ecffdf7b1efccf9ea6f8abde95ad585a19f36451
Shah Meer Ali(Sensei) <101212857+Shahmeerali723@users.noreply.github.com>
2022-10-05 04:24:46 -0700

    Add files via upload

---
+++ Task-29./index.js
@@ -0,0 +1,11 @@
+/* Checking if the array includes the fruit. */
+let favorite_fruits = ["Grapes", "Pomegranate", "Banana", "Apple"];
+
+if (favorite_fruits.includes("Grapes"))
+console.log(`I You really like Grapes!`);
+if (favorite_fruits.includes("Pomegranate"))
+console.log(`I really like Pomegrenate!`);
+if (favorite_fruits.includes("Banana"))
+console.log(`I You really like Banana!`);
+if (favorite_fruits.includes("Grapes")) console.log(`I never run`);
+if (favorite_fruits.includes("mangol")) console.log(`I never run`);
