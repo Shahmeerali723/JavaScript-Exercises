d67654933fbf50365029b80553aa79232b785f93
Shah Meer Ali(Sensei) <101212857+Shahmeerali723@users.noreply.github.com>
2022-10-05 04:23:00 -0700

    Add files via upload

---
+++ Task-27./index.js
@@ -0,0 +1,6 @@
+/* Checking the value of alien_color and printing a message based on the value. */
+
+let alien_color = "green";
+if (alien_color === "green") console.log("player just earned 5 points");
+else if (alien_color === "yellow") console.log("player just earned 10 points");
+else if (alien_color === "red") console.log("player just earned 15 points");
