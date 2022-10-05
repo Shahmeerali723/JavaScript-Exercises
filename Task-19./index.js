a120e5c6a25768411203e2789f0db6f6221a4de7
Shah Meer Ali(Sensei) <101212857+Shahmeerali723@users.noreply.github.com>
2022-10-05 04:14:16 -0700

    Add files via upload

---
+++ Task-19./index.js
@@ -0,0 +1,6 @@
+/* Declaring a variable named guest and assigning it an array of strings.
+Then it is logging the length of the array to the console. */
+
+
+let guest = ["Shah Meer", "Sulman", "Laraib", "Wajid", "wajahat", "Ibdar"];
+console.log(guest.length);
---
+++ Task-19./readme.md
@@ -0,0 +1,6 @@
+# PIAIC Assignment Javascript
+
+Piaic Web 3.o 0r Metaverse.
+
+Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number
+of people you are inviting to dinner.
