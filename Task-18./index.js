b4f08a1ee3f3f259091cf79a7fba542c7d6c17c6
Shah Meer Ali(Sensei) <101212857+Shahmeerali723@users.noreply.github.com>
2022-10-05 04:13:21 -0700

    Add files via upload

---
+++ Task-18./index.js
@@ -0,0 +1,12 @@
+/* The above code is sorting the array in ascending order and then reversing it to get the descending
+order. */
+let locations = ["Egypt", "Germany", "Russia", "Dubai", "United Nation", "London", "Italy"];
+console.log("Location: ", locations);
+console.log("Sorted Location: ", locations.slice().sort());
+console.log("Original Location: ", locations);
+console.log("Sorted Reverse Location: ", locations.slice().sort().reverse());
+console.log("Original Location: ", locations);
+console.log("Reversed Order of Location: ", locations.reverse());
+console.log("Re-Reversed Order of Location: ", locations.reverse());
+console.log("Sorted Location: ", locations.sort());
+console.log("Sorted Reverse Location: ", locations.reverse());
