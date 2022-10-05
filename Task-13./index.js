186ab9adfe06526c0199993505ec19566fc19fd9
Shah Meer Ali(Sensei) <101212857+Shahmeerali723@users.noreply.github.com>
2022-10-05 04:08:32 -0700

    Add files via upload

---
+++ Task-13./index.js
@@ -0,0 +1,4 @@
+/* Creating an array of transport and then logging the first item in the array. */
+
+const transportation = ["Royal Infanity", "Royal Royce", "Royal Wonder of the seas", "Lockheed sr-71 Blackbird"];
+transportation.map((transport) => console.log(`I would like to own a ${transport}`));
