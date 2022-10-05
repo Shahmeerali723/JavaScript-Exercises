395bbbe994f020ea5281783b644317f4c3b7917e
Shah Meer Ali(Sensei) <101212857+Shahmeerali723@users.noreply.github.com>
2022-10-05 04:36:09 -0700

    Add files via upload

---
+++ Task-41./index.js
@@ -0,0 +1,8 @@
+/**
+ * The function show_magicians() takes an array of strings as an argument and prints each string in the
+ */
+const magician = ["zee", "tyler", "halloween" , "john", "joe", "jim"];
+const show_magicians = (arr) => {
+arr.map((idx) => console.log(idx));
+};
+show_magicians(magician);
---
+++ Task-41./readme.md
@@ -0,0 +1,6 @@
+# PIAIC Assignment Javascript.
+
+Piaic We 3.O or Meta verse
+
+Magicians: Make a array of magician’s names. Pass the array to a function
+called show_magicians(), which prints the name of each magician in the array.
