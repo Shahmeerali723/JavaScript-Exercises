93dd212a0b928979014f324a4be750e21f287853
Shah Meer Ali(Sensei) <101212857+Shahmeerali723@users.noreply.github.com>
2022-10-05 03:53:04 -0700

    Add files via upload

---
+++ Task-06./index.js
@@ -0,0 +1,6 @@
+/*
+Print the name once, whitespace around the name is displayed. Then print the name after striping the white spaces.
+*/
+
+Myfav_name = "Sensei";
+console.log(`${Myfav_name}\t is my favourite name.`);
