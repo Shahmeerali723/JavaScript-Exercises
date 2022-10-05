cf336d383e669770c505cdb3c122e52a56ee4fc2
Shah Meer Ali(Sensei) <101212857+Shahmeerali723@users.noreply.github.com>
2022-10-05 04:09:31 -0700

    Add files via upload

---
+++ Task-14./index.js
@@ -0,0 +1,5 @@
+/* The above code is creating a list of friends and inviting them to dinner. */
+
+
+let guests = ["Sulman", "Laraib", "Qalb-E-Momin"];
+guests.map((guest)=> console.log(`Dear ${guest}, have for you dinner with me`));
