17290cd9be754bb24e180330118a46d612cdaa48
Shah Meer Ali(Sensei) <101212857+Shahmeerali723@users.noreply.github.com>
2022-10-05 04:22:19 -0700

    Add files via upload

---
+++ Task-26./index.js
@@ -0,0 +1,6 @@
+/* Checking if the alien color is green and if it is it will print out the first console.log. If it is
+not green it will print out the second console.log. */
+let alien_color = "green";
+if (alien_color === "green")
+console.log("player just earned 5 points for shooting the alien.");
+else console.log("player just earned 10 points");
